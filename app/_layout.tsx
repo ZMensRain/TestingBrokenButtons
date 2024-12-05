1b69ad44b92e7121caa0d61912a95ebbf48ecc9b
ZMensRain <116457838+ZMensRain@users.noreply.github.com>
2024-12-05 09:45:07 +0200

    workaround

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,26 @@
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { Stack } from "expo-router";
+import {
+  GestureHandlerRootView,
+  TouchableWithoutFeedback,
+} from "react-native-gesture-handler";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{
-          //When the icon is pressed it will sometimes not register visually and will very often not call the function
-          headerRight: () => (
-            <MaterialCommunityIcons
-              name="plus"
-              size={30}
-              onPress={() => alert("it worked")}
-            />
-          ),
-        }}
-      />
-    </Stack>
+    <GestureHandlerRootView>
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{
+            //When the icon is pressed it will sometimes not register visually and will very often not call the function
+            headerRight: () => (
+              <TouchableWithoutFeedback onPress={() => alert("it worked")}>
+                <MaterialCommunityIcons name="plus" size={30} />
+              </TouchableWithoutFeedback>
+            ),
+          }}
+        />
+      </Stack>
+    </GestureHandlerRootView>
   );
 }
