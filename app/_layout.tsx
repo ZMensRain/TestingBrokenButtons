223488ecd33831243a01c45e8a9d23862da3b7d1
ZMensRain <116457838+ZMensRain@users.noreply.github.com>
2024-12-05 09:32:02 +0200

    added a icon button to the title bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,22 @@
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { Stack } from "expo-router";
-import { Button } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          //When the icon is pressed it will sometimes not register visually and will very often not call the function
+          headerRight: () => (
+            <MaterialCommunityIcons
+              name="plus"
+              size={30}
+              onPress={() => alert("it worked")}
+            />
+          ),
+        }}
+      />
+    </Stack>
+  );
 }
